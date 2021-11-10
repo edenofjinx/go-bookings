a75030965495c16323f13b3b3d75eed9bc596868
Julius Liaudanskis <julius.l@emotion.lt>
2021-11-10 15:40:50 +0200

    go app: 105

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -38,7 +38,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) 
 }
 
 // InsertRoomRestriction insert a room restriction into the database
-func (m* postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 	stmt := `insert into room_restrictions
@@ -59,4 +59,22 @@ func (m* postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 		return err
 	}
 	return nil
+}
+
+// SearchAvailabilityByDates returns true if availability exists for roomId and false if no availability exists
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomId int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	var numRows int
+	query := `select count(id) from room_restrictions
+				where room_id = $1 and $2 < end_date and $3 > start_date;`
+	row := m.DB.QueryRowContext(ctx, query, roomId, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+	if numRows == 0 {
+		return true, nil
+	}
+	return false, nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,13 @@
 package repository
 
-import "github.com/edenofjinx/go-bookings/internal/models"
+import (
+	"github.com/edenofjinx/go-bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomId int) (bool, error)
 }
